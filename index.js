68e0bda5f56898477bb4a789fe1ae7fd2a0c1b96
Itzkirimi <kirimihansel@gmail.com>
2024-01-16 04:42:56 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
-}
+   const inputElement = document.querySelector('#input');
+       inputElement.addEventListener('input', (event) => {
+           console.log('The input element was updated with the value:', event.target.value);
+        });
+       }
+       
+       addingEventListener();
+
+
+
